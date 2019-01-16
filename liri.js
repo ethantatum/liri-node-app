660e8294ae62c0b84ea3fd1e0f630be0c907b45c
animus apertus <37455165+ethantatum@users.noreply.github.com>
2019-01-16 15:26:55 -0700

    added concert-this functionality

--- liri.js
+++ liri.js
@@ -2,10 +2,15 @@ require('dotenv').config();
 
 const keys = require('./keys.js');
 
-const Spotify = require('node-spotify-api');
-const spotify = new Spotify(keys.spotify);
-
 const axios = require('axios');
+
+const moment = require('moment');
+
+
+// const Spotify = require('node-spotify-api');
+// const spotify = new Spotify(keys.spotify);
+// console.log(spotify);
+
 
 let nodeArgs = process.argv;
 
@@ -26,12 +31,17 @@ if(process.argv[2] === `concert-this`) {
     console.log(concertUrl);
     
     axios.get(concertUrl).then(
-        function(response, body) {
-            console.log(response);
-            let userArtist = JSON.parse(body);
-          console.log(`Venue: ${userArtist.venue.name}`);
-          console.log(`Location: ${userArtist.venue.city}, ${userArtist.venue.region}`);
-          console.log(`Date: ${userArtist.datetime}`);
+        function(response) {
+            if(response.data[0] === undefined) {
+                console.log(`Oh no! No upcoming events for that artist...try another one!`);
+            }
+            else {
+            console.log(`Venue: ${response.data[0].venue.name}`);
+            console.log(`Location: ${response.data[0].venue.city} ${response.data[0].venue.region} ${response.data[0].venue.country}`);
+            console.log(`Date: ${response.data[0].datetime}`);
+            }
         }
-      );
+      ).catch(function (error) {
+        console.log(`This is axios${error}`);
+      });
 }
